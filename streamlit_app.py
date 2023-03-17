e27ae963855e22fcd8d741529c8b3f3ea9471e6e
Mihir Padsumbiya <64645699+mihirp381@users.noreply.github.com>
2023-03-17 12:50:58 -0500

    Update streamlit_app.py
    Completed the steps for Lesson3_1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
